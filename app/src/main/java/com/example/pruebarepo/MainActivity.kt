7d30c1789516254cd3b5ccbb123e8266c9c8f016
TRUENO59 <hmanrique@hotmail.com>
2023-01-03 11:56:19 -0400

    03012023 10:24 am (primer commit)

---
+++ app/src/main/java/com/example/pruebarepo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.pruebarepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println ("Esto es un ejemplo-1   ******************")
+        println ("Esto es un ejemplo-2   ******************")
+        //val text=TextView("UUUUUUU")
+    }
+}
